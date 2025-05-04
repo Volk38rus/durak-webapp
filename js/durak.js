7b3ae406f4ab9f3a4a98c9a1539d15d6f05e8744
Volk38rus <permyakov38rus@gmail.com>
2025-05-04 20:42:19 +0300

    Update durak.js

--- js/durak.js
+++ js/durak.js
@@ -3,18 +3,39 @@ function getGameIdFromUrl() {
     return params.get("game_id") || "UNKNOWN";
 }
 
-function sendMove() {
+function sendCard(card) {
     const gameId = getGameIdFromUrl();
-    const moveData = {
+    const data = {
         action: "move",
-        card: "6♠",
+        card: card,
         game_id: gameId
     };
-    Telegram.WebApp.sendData(JSON.stringify(moveData));
+    Telegram.WebApp.sendData(JSON.stringify(data));
+}
+
+function generateCards() {
+    const suits = ["♠", "♥", "♦", "♣"];
+    const ranks = ["6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+    const hand = new Set();
+    while (hand.size < 6) {
+        const card = ranks[Math.floor(Math.random() * ranks.length)] + suits[Math.floor(Math.random() * suits.length)];
+        hand.add(card);
+    }
+    return [...hand];
 }
 
 window.onload = function () {
+    Telegram.WebApp.ready();
     const gameId = getGameIdFromUrl();
     document.getElementById("status").innerText = "Вы подключены к игре № " + gameId;
-    Telegram.WebApp.ready();
+
+    const cards = generateCards();
+    const container = document.getElementById("cardContainer");
+    cards.forEach(card => {
+        const div = document.createElement("div");
+        div.className = "card";
+        div.innerText = card;
+        div.onclick = () => sendCard(card);
+        container.appendChild(div);
+    });
 };
